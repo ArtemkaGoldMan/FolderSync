4547bf059a804c3c4dcc678df4d320c8943d90aa
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-04-06 21:42:34 +0200

    define interfaces

---
+++ Folderify/Folderify.App/Services/Interfaces/IFolderComparer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folderify.App.Services.Interfaces
+{
+    public interface IFolderComparer
+    {
+        IEnumerable<string> GetFilesToCopy();
+        IEnumerable<string> GetFilesToDelete();
+    }
+}
---
+++ Folderify/Folderify.App/Services/Interfaces/IScheduler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folderify.App.Services.Interfaces
+{
+    public interface IScheduler
+    {
+        void Start();
+    }
+}

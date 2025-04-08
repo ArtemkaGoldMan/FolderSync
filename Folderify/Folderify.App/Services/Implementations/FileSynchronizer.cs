fc73e6df1b8ea00e34a6aaa830dc621a8906ef02
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-04-08 11:04:58 +0200

    Implement Scheduler | delete usings '

--- Folderify/Folderify.App/Services/Implementations/FileSynchronizer.cs
+++ Folderify/Folderify.App/Services/Implementations/FileSynchronizer.cs
@@ -1,9 +1,4 @@
 ﻿using Folderify.App.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Folderify.App.Services.Implementations
 {
---
+++ Folderify/Folderify.App/Services/Implementations/PeriodicScheduler.cs
@@ -0,0 +1,27 @@
+﻿using Folderify.App.Services.Interfaces;
+
+namespace Folderify.App.Services.Implementations
+{
+    public class PeriodicScheduler : IScheduler
+    {
+        private readonly IFileSynchronizer _synchronizer;
+        private readonly ILogger _logger;
+        private readonly int _interval;
+        private Timer? _timer;
+
+        public PeriodicScheduler(IFileSynchronizer synchronizer, ILogger logger, int interval)
+        {
+            _synchronizer = synchronizer;
+            _logger = logger;
+            _interval = interval * 1000;
+        }
+
+        public void Start()
+        {
+            _logger.Log("Starting folder synchronization scheduler...");
+            _timer = new Timer(_ => _synchronizer.Synchronize(), null, 0, _interval);
+            Console.ReadLine(); // keep app running
+        }
+    }
+
+}
